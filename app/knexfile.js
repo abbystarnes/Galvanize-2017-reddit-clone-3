f6b1dd14a86ed783f9cb0eaca5319fbfc9f130c1
Abby Starnes <abbymstarnes@gmail.com>
2017-07-20 14:50:24 -0500

    remove db connection

--- app/knexfile.js
+++ app/knexfile.js
@@ -3,8 +3,7 @@ module.exports = {
   development: {
     client: 'pg',
     connection: {
-      database: process.env.DATABASE_URL || 'reddit-clone',
-      connection: 'postgres://localhost/reddit-clone'
+      database: process.env.DATABASE_URL || 'reddit-clone'
     }
   },
 
