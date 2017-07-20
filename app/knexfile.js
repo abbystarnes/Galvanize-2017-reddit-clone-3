5bf356668ef564a3efb67106d6f51a224b16d1fe
Abby Starnes <abbymstarnes@gmail.com>
2017-07-20 15:30:21 -0500

    updates

--- app/knexfile.js
+++ app/knexfile.js
@@ -7,6 +7,11 @@ module.exports = {
     }
   },
 
+  production: {
+    client: 'pg',
+    connection: `${process.env.DATABASE_URL}?ssl=true`
+  },
+
   test: {
     client: 'pg',
     connection: {
